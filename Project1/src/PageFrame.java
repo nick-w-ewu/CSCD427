68d664ee531acdd6ae937c06e119faad59011297
nick-w-ewu <nicholas.witmer@eagles.ewu.edu>
2016-04-03 15:03:21 -0700

    project 1 started

---
+++ Project1/src/PageFrame.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+/**
+ * Created by nicho on 3/31/2016.
+ */
+public class PageFrame
+{
+    private int frameNum;
+    private int pageNumber;
+    private int pinCount;
+    private ArrayList<String> contents;
+
+}
